8e591114f04d42135fe9f4002daf04c69383f04e
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-15 19:06:50 +0200

    testcheers

--- Cheers.java
+++ Cheers.java
@@ -3,7 +3,7 @@ public class Cheers {
         public static void main(String[] args) {
                 // Replace this comment with your code
                 char[] arrChar = { 'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X' };
-                String name = args[0];
+                String name = args[0].toUpperCase();
                 int wow = Integer.parseInt(args[1]);
                 // lop for the str
                 for (int i = 0; i < name.length(); i++) {
