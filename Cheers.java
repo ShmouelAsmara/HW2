09d22c19120be1f62a495f33b31b435d7cb3473d
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-15 19:02:19 +0200

    testcheers

--- Cheers.java
+++ Cheers.java
@@ -9,14 +9,14 @@ public class Cheers {
                 for (int i = 0; i < name.length(); i++) {
                         boolean printTest = true;
                         for (int X = 0; X < arrChar.length; X++) {
-
                                 if (name.indexOf(arrChar[X], i) == i) {
-                                        System.out.println("Give me an" + name.charAt(0 + i) + "!");
+                                        System.out.println("Give me an " + name.charAt(0 + i) + ": "
+                                                        + name.charAt(0 + i) + "!");
                                         printTest = false;
                                 }
                         }
                         if (printTest == true) {
-                                System.out.println("Give me a " + name.charAt(0 + i) + "!");
+                                System.out.println("Give me a " + name.charAt(0 + i) + ": " + name.charAt(0 + i) + "!");
                         }
                 }
                 System.out.println("What does that spell?");
