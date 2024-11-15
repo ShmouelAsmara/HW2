6c34cb842ec30a2d799ed00fbe92f93f7faab6cc
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-15 13:38:14 +0200

    TimecalcTest

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,23 +1,24 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-        int hour = Integer.parseInt(args[0]);
-        int minutes = Integer.parseInt(args[1]);
-        int minutesToAdd = Integer.parseInt(args[2]);
+        // reminder to check how i can get the hh:mm
+        int hour = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
 
         // clac for the min and ex for hours
         int totalMinutes = (hour * 60) + minutes + minutesToAdd;
         // calc hour
+
         int calcHour = (totalMinutes / 60);
         int newHour = calcHour % 24;
         // calc minutes
         int newMinutes = totalMinutes - (calcHour * 60);
         if (newHour < 9) {
-            if (newMinutes < 9) {
+            if (newMinutes <= 9) {
                 System.out.println("0" + newHour + ":0" + newMinutes);
                 return;
             }
-            System.out.println("0" + newHour + newMinutes);
+            System.out.println("0" + newHour + ":" + newMinutes);
             return;
         }
         if (newMinutes < 9) {
