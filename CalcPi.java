a4468003ce2e0321030e5ad3a49f3c0cef7fb305
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-17 13:09:16 +0200

    testPi

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String[] args) {
+		// Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+		double pi = 0.0;
+
+		for (int i = 0; i < number; i++) {
+			double sum = (1.0 / ((2 * i) + 1));
+			if ((i % 2) == 0) {
+				pi = pi + sum;
+			} else {
+				pi = pi - sum;
+			}
+
+		}
+		pi = pi * 4;
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + pi);
+
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,50 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		// Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+		// String mode = args[1];
+		// int[] myArry = {};
+		int test = number;
+		int order = 1;
+
+		for (int i = 1; i < test; i++)
+			System.out.print(order + "");
+		for (int x = 0; order == 1; x++) {
+			if ((order % 2) == 0) {
+				order = order / 2;
+				System.out.print(order);
+
+			} else {
+				order = (order * 3) + 1;
+				System.out.print(order);
+			}
+		}
+
+		// for (int i = 0; i < test; i++) {
+		// System.out.println("");
+		// System.out.print("");
+
+		// System.out.print(" (" + x + ")");
+		// }
+
+		// if (mode == "c") {
+		// }
 	}
 }
+
+/*
+ * if (test == 1) {
+ * for(int x=0; order >1; x++){
+ * if ((order % 2) == 0) {
+ * order = order / 2;
+ * System.out.print(order);
+ * 
+ * } else {
+ * order = (order * 3) + 1;
+ * System.out.print(order);
+ * }
+ * }
+ * 
+ * }
+ */
