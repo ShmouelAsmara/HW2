23f74035dcd3b046ed0602e0ce80d6e325681bf6
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-17 22:18:47 +0200

    test Collatz

--- Collatz.java
+++ Collatz.java
@@ -3,48 +3,48 @@ public class Collatz {
 	public static void main(String args[]) {
 		// Replace this comment with your code
 		int number = Integer.parseInt(args[0]);
-		// String mode = args[1];
-		// int[] myArry = {};
-		int test = number;
-		int order = 1;
+		String mode = args[1].toUpperCase();
+		int numberFinal = number;
+		int counter = 1;
+		int tempOne;
+		if (mode.equals("V")) {
+			// loop for the number of the line
+			for (int i = 1; i <= number; i++) {
+				System.out.print(i);
+				// lop for dealing with one
+				if (i == 1) {
+					tempOne = (i * 3) + 1;
+					System.out.print(tempOne);
+					number = tempOne;
 
-		for (int i = 1; i < test; i++)
-			System.out.print(order + "");
-		for (int x = 0; order == 1; x++) {
-			if ((order % 2) == 0) {
-				order = order / 2;
-				System.out.print(order);
+				} else {
+					number = i;
+				}
+				// lop that stop when I'm in num one
+				while (number != 1) {
+					if ((number % 2) == 0) {
+						number = number / 2;
+						System.out.print(number);
+					} else {
+						number = ((number * 3) + 1);
+						System.out.print(number);
+					}
+					counter = counter + 1;
 
-			} else {
-				order = (order * 3) + 1;
-				System.out.print(order);
+				}
+				System.out.println(" (" + counter + ")");
+				System.out.println("");
+
+				// bring the counter and the number for the start
+				number = Integer.parseInt(args[0]);
+				counter = 1;
 			}
+
+			System.out.println("Every one of the first " + numberFinal + " hailstone sequences reached 1.");
+		} else {
+
+			System.out.println("Every one of the first " + numberFinal + " hailstone sequences reached 1.");
 		}
 
-		// for (int i = 0; i < test; i++) {
-		// System.out.println("");
-		// System.out.print("");
-
-		// System.out.print(" (" + x + ")");
-		// }
-
-		// if (mode == "c") {
-		// }
 	}
 }
-
-/*
- * if (test == 1) {
- * for(int x=0; order >1; x++){
- * if ((order % 2) == 0) {
- * order = order / 2;
- * System.out.print(order);
- * 
- * } else {
- * order = (order * 3) + 1;
- * System.out.print(order);
- * }
- * }
- * 
- * }
- */
